a6095f72eb734cef193dfdaef7a11ff7f33795ea
alex <631180821@qq.com>
2024-04-29 17:04:55 +0800

    120 移动0

--- src/main/java/leetcode/editor/cn/[283]移动零.java
+++ src/main/java/leetcode/editor/cn/[283]移动零.java
@@ -5,7 +5,17 @@ import leetcode.editor.cn.template.TreeNode;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public void moveZeroes(int[] nums) {
-
+        int fast =0,slow=0,n=nums.length;
+        while (fast<n){
+            if (nums[fast]!=0){
+                nums[slow]=nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        for (int i = slow; i < n; i++) {
+            nums[i]=0;
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
