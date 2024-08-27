71c8bbfe3aaf11a2cc1e53546d9630b3c25c458a
alex <631180821@qq.com>
2024-08-27 09:26:04 +0800

    8.27

---
+++ src/main/java/leetcode/editor/cn/Subsets.java
@@ -0,0 +1,18 @@
+
+  package leetcode.editor.cn;
+
+  import java.util.List;
+
+  public class Subsets{
+      public static void main(String[] args) {
+           Solution solution = new Subsets().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
--- src/main/java/leetcode/editor/cn/[2732]找到矩阵中的好子集.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public List<Integer> goodSubsetofBinaryMatrix(int[][] grid) {
-
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
