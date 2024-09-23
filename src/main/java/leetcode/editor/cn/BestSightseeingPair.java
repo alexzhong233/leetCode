b5b43d989570a9eaff894dbf8c94585fe100a332
alex <631180821@qq.com>
2024-09-23 17:13:39 +0800

    9.23

---
+++ src/main/java/leetcode/editor/cn/BestSightseeingPair.java
@@ -0,0 +1,22 @@
+
+  package leetcode.editor.cn;
+  import java.util.ArrayList;
+  import java.util.Arrays;
+  import java.util.List;
+  import leetcode.editor.cn.utils.TreeNode;
+  import leetcode.editor.cn.utils.ListNode;
+  
+  
+  public class BestSightseeingPair{
+      public static void main(String[] args) {
+           Solution solution = new BestSightseeingPair().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
---
+++ src/main/java/leetcode/editor/cn/LengthOfTheLongestAlphabeticalContinuousSubstring.java
@@ -0,0 +1,22 @@
+
+  package leetcode.editor.cn;
+  import java.util.ArrayList;
+  import java.util.Arrays;
+  import java.util.List;
+  import leetcode.editor.cn.utils.TreeNode;
+  import leetcode.editor.cn.utils.ListNode;
+  
+  
+  public class LengthOfTheLongestAlphabeticalContinuousSubstring{
+      public static void main(String[] args) {
+           Solution solution = new LengthOfTheLongestAlphabeticalContinuousSubstring().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int longestContinuousSubstring(String s) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
